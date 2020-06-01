f2101ef686b9fc176bb18594a9c13e51137a18d4
pedromaximilian <pedro.maximilian@gmail.com>
2020-06-01 14:44:00 -0300

    Update ApiClient.java

--- app/src/main/java/com/example/loginmvvm/request/ApiClient.java
+++ app/src/main/java/com/example/loginmvvm/request/ApiClient.java
@@ -3,63 +3,92 @@ package com.example.loginmvvm.request;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.loginmvvm.model.Usuario;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class ApiClient {
     private static SharedPreferences sp;
 
-    private static SharedPreferences conectar(Context context){
-        if (sp == null){
-            sp = context.getSharedPreferences("datos", 0);
-        }
-        return sp;
-    }
+
 
     public static void guardar(Context context, Usuario usuario){
-        SharedPreferences sp = conectar(context);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putLong("dni", usuario.getDni());
-        editor.putString("apellido", usuario.getApellidos());
-        editor.putString("nombre", usuario.getNombre());
-        editor.putString("mail", usuario.getMail());
-        editor.putString("pass", usuario.getPass());
-        editor.commit();
+        try {
+            File archivo = new File (context.getFilesDir(), "misdatos.dat");
+            FileOutputStream fo = new FileOutputStream(archivo);
+            BufferedOutputStream bo = new BufferedOutputStream(fo);
+            ObjectOutputStream oos = new ObjectOutputStream(bo);
+
+            oos.writeObject(usuario);
+
+            bo.flush();
+            fo.close();
+
+        } catch (IOException io) {
+            Toast.makeText(context, "Error al guardar", Toast.LENGTH_LONG).show();
+        }
     }
 
     public static Usuario leer(Context context){
-        SharedPreferences sp = conectar(context);
-        Long dni = sp.getLong("dni", -1);
-        String apellido = sp.getString("apellido", "-1");
-        String nombre = sp.getString("nombre", "-1");
-        String mail = sp.getString("mail", "-1");
-        String pass = sp.getString("pass", "-1");
+        Usuario usuario = null;
 
-        Usuario usuario = new Usuario(dni, apellido, nombre, mail, pass);
+        try {
+            File archivo = new File (context.getFilesDir(), "misdatos.dat");
+            FileInputStream fi = new FileInputStream(archivo);
+            BufferedInputStream bi = new BufferedInputStream(fi);
+            ObjectInputStream ois = new ObjectInputStream(bi);
+
+            usuario = (Usuario) ois.readObject();
+
+            fi.close();
+
+        } catch (IOException io) {
+            Toast.makeText(context, "No se encontró el archivo", Toast.LENGTH_LONG).show();
+        } catch (ClassNotFoundException cnfe) {
+            Toast.makeText(context, "No se encontró el objeto", Toast.LENGTH_LONG).show();
+        }
+
         return usuario;
     }
 
-    public static Usuario login(Context context, String mailInput, String passInput){
-        Usuario usuario= null;
-        SharedPreferences sp = conectar(context);
-        Long dni = sp.getLong("dni", -1);
-        String apellido = sp.getString("apellido", "-1");
-        String nombre = sp.getString("nombre", "-1");
-        String mail = sp.getString("mail", "-1");
-        String pass = sp.getString("pass", "-1");
+    public static Usuario login(Context context, String mailInput, String passInput) {
+        Usuario usuario = null;
 
-        if (mail.equals(mailInput) && pass.equals(passInput)){
-            usuario = new Usuario(dni, apellido, nombre, mail, pass );
-            return usuario;
-        }else{
-            return usuario;
+        try {
+            File archivo = new File(context.getFilesDir(), "misdatos.dat");
+            FileInputStream fi = new FileInputStream(archivo);
+            BufferedInputStream bi = new BufferedInputStream(fi);
+            ObjectInputStream ois = new ObjectInputStream(bi);
+
+            usuario = (Usuario) ois.readObject();
+
+            fi.close();
+            if (mailInput.equals(usuario.getMail()) && passInput.equals(usuario.getPass())) {
+                return usuario;
+            }
+
+        } catch (IOException io) {
+            //Toast.makeText(context, io.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (ClassNotFoundException ex) {
+            //Toast.makeText(context, ex.getMessage(), Toast.LENGTH_LONG).show();
         }
 
+        return null;
+
+
+    }
 
     }
 
 
 
 
-
-}
